9dbe10f56880e6e80ff923158d79e32612bfe1c5
adillari1493 <adil.lari1493@gmail.com>
2024-08-08 21:21:45 +0530

    add switch file

---
+++ src/main/java/Statements/SwitchDays.java
@@ -0,0 +1,31 @@
+package Statements;
+
+public class SwitchDays {
+    public static void main(String[] args) {
+        int n = 7;
+        switch (n)
+        {
+            case 1:
+                System.out.println("It's Monday");
+                break;
+            case 2:
+                System.out.println("It's Tuesday");
+                break;
+            case 3:
+                System.out.println("It's Wednesday");
+                break;
+            case 4:
+                System.out.println("It's Thursday!");
+                break;
+            case 5:
+                System.out.println("It's Friday");
+                break;
+            case 6:
+                System.out.println("It's Saturday");
+                break;
+            case 7:
+                System.out.println("Wow!! It's Sunday");
+                break;
+        }
+    }
+}
